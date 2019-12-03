9247f70c233b846f13453527270d6f1b03e2103c
Victor El-Hajj <elhajj@student.chalmers.se>
2019-12-03 16:56:13 +0100

    Completed day 2

--- 3/a.hs
+++ 3/a.hs
@@ -7,7 +7,7 @@ main = do
         sndSizes      = map wireToSize $ wires !! 1
         fstPoints     = wirePoints fstSizes
         sndPoints     = wirePoints sndSizes
-    print $ minimum $ map manhattanDistance $ intersect fstPoints sndPoints
+    print $ minimum $ map manhattanDistance $ delete(0,0) $ intersect fstPoints sndPoints
 
 wireToSize :: String -> (Integer, Integer)
 wireToSize ('R':s) = ( read s :: Integer, 0)
@@ -17,6 +17,7 @@ wireToSize ('D':s) = (0, -read s :: Integer)
 wireToSize _ = error "Unknown wire segment"
 
 -- Range that does not return empty list of a>b
+range :: (Ord a, Enum a) => a -> a -> [a]
 range a b | a<b = [a..b]
           | a>b = [b..a]
           | otherwise = [a]
@@ -28,3 +29,6 @@ manhattanDistance (x, y) = (abs x) + (abs y)
 -- Traverse and save all points of the wire
 wirePoints :: [(Integer, Integer)] -> [(Integer, Integer)]
 wirePoints = delete (0,0) . foldl (\acc@((ax, ay):_) (x, y) -> (ax+x, ay+y):(acc++[(a, b) | a <- range ax (ax+x), b <- range ay (ay+y)])) [(0,0)] 
+
+
+-- This solution is really bad. Takes ~15 mins to solve. For a MUCH better solution see day 3 part 2, that solves in about a second.
